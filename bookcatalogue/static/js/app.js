fbba55d0ad6042d546a5b4e7f5ec57910a0d726f
Vidian Vito <vidian@3ecpa.com.sg>
2023-09-01 09:51:30 +0700

    GET, POST,PUT(not work correctly), DELETE

--- bookcatalogue/static/js/app.js
+++ bookcatalogue/static/js/app.js
@@ -8,6 +8,17 @@ app.controller('bookCtrl',function($scope, $http){
         $scope.books = data.data;
     })
 
+    $scope.saveIdBook = function(id){
+        $scope.idBook = id;
+    }
+
+    $scope.getBook = function(id){
+        $scope.idEdit = id
+        $http.get("http://127.0.0.1:8000/api/book/" + $scope.idEdit + "/").then(function (book_data){
+            $scope.bookById = book_data.book_data;
+        })
+    }
+
     $scope.postBook = function(){
         let url = 'http://127.0.0.1:8000/api/book/'
         $http.post(url, JSON.stringify($scope.data)).then(function success(response){
@@ -16,9 +27,14 @@ app.controller('bookCtrl',function($scope, $http){
         })
     }
 
-    $scope.saveIdBook = function(id){
-        $scope.idBook = id;
+    $scope.updateBook = function(){
+        let url = 'http://127.0.0.1:8000/api/book/'
+        $http.post(url, JSON.stringify($scope.data)).then(function success(response){
+            alert('Success');
+            location.reload();
+        })
     }
+
 
     $scope.deleteBook = function(){        
         $http.delete("http://127.0.0.1:8000/api/book/" + $scope.idBook + "/")
